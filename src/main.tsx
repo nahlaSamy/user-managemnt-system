4ede0ed885fdce12e0833b35ddbb6260394f94e7
Nahla samy <Nahlasamy583@gmail.com>
2024-06-05 18:29:23 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import '@fortawesome/fontawesome-free/css/all.min.css'
-import "bootstrap/dist/js/bootstrap.bundle.js"
-import "bootstrap/dist/css/bootstrap.min.css"
-import App from './components/APP/App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from './components/APP/App'; 
 
-import './index.css'
+import './index.css';
+import AuthContextProvider from './components/Context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
+  </React.StrictMode>
+);
