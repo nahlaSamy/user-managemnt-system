76a1de16604996c03fdf4a4f0225b3250e1f2eaf
Nahla samy <Nahlasamy583@gmail.com>
2024-06-05 18:29:12 +0300

    Update SideBar.tsx

--- src/components/common/SideBar/SideBar.tsx
+++ src/components/common/SideBar/SideBar.tsx
@@ -1,8 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState,useContext } from 'react'
 import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
 import profilephoto from "../../../assets/imgs/profile-photo.png"
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Context/AuthContext';
+
 export default function SideBar() {
+  let {userData}=useContext(AuthContext)
+
   const [isCollapse,setIsCollapse]=useState(false)
   const toggleCollapse =()=>{
     setIsCollapse(!isCollapse)
@@ -16,8 +20,8 @@ export default function SideBar() {
       <MenuItem icon={<i className='fa fa-bars'></i>} onClick={toggleCollapse} >  </MenuItem>
 
         <div className="profileData m-5">
-        <img  className="rounded-circle" src={profilephoto} alt="" />
-        <h1 className='h6 my-3'> Karthi Madesh</h1>
+        <img  className="rounded-circle" src={userData?.image} alt="" />
+        <h1 className='h6 my-3'> {userData?.firstName} {userData?.lastName}</h1>
           </div>
       <MenuItem icon={<i className='fa fa-home'></i>} component={<Link to ="/home"/>} > Home </MenuItem>
         <MenuItem icon={<i className='fa fa-users'></i>} component={<Link to ="/home/userslist"/>} > users </MenuItem>
